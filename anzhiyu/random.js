9de71819c232ee26ad5444db13e75ecfbdd29366
Alenloky <3235221427@qq.com>
2025-03-27 11:39:33 +0800

    Site updated: 2025-03-27 11:39:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/26/记事本的保存功能/","2025/03/26/hello-world/"];function toRandomPost(){
+var posts=["2025/03/26/hello-world/","2025/03/26/记事本的保存功能/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
