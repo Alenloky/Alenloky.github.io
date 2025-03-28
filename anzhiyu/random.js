9a68cfc57d398230902765da0c5c8e88cfbb5454
Alenloky <3235221427@qq.com>
2025-03-29 02:49:16 +0800

    Site updated: 2025-03-29 02:49:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/26/记事本的保存功能/","2025/03/26/hello-world/","2025/03/28/大二的一次打工跑腿经历/","2025/03/27/csapp学习进度/"];function toRandomPost(){
+var posts=["2025/03/27/csapp学习进度/","2025/03/26/hello-world/","2025/03/26/记事本的保存功能/","2025/03/28/大二的一次打工跑腿经历/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
